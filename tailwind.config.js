59771334ddb3c7ccb025b2119531782dd1d8516e
Mehedi Zaman <mehedi.zaman.professional@gmail.com>
2024-08-21 02:27:35 +0600

    Half project done with responsive

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          primary_color: '#FF4240',
+        }
+      }
+    }
+  }
